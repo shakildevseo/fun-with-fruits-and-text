4e6d70743cc8ad3e83f2d7addb9a88ed636b93e8
shakildevseoo <shakildevseoo@gmail.com>
2025-12-18 18:04:18 +0600

    fun-with-text component done

--- src/components/FunWithText/FunWithText.jsx
+++ src/components/FunWithText/FunWithText.jsx
@@ -1,22 +1,78 @@
 import React from 'react'
 import "../../App.css"
+import { useState } from 'react'
 const FunWithText = () => {
 
+// state management start
+const [firstName, setFirstName] = useState("")
+
+const [lastName, setLastName] = useState("")
+
+const [fullName, setFullName] = useState("Show Preview Output 😁🫵👉 : ")
+// state management end
 
 
+// handle function start
+ const handlerFirstName = (e)=>{
+   setFirstName(e.target.value)
+}
+ const handlerLastName = (e)=>{
+   setLastName(e.target.value)
+}
 
-    
+
+ 
+
+// handle function end
+
+
+// function start
+  const handleClick = ()=>{
+       function showFullName(){
+           setFullName(`Full Name:  ${firstName} ${lastName} ` );
+
+           setFirstName("");
+           setLastName("")
+
+       }
+       showFullName()
+
+       //or
+       /* 
+       
+       function showFullName(){
+           setFullName(`Full Name:  ${firstName} ${lastName} ` );
+
+           setFirstName("");
+           setLastName("")
+
+       }
+    handlerClick(){
+   showFullName()
+    }
+       
+       */
+  }
+
+// function end
+
+
   return (
     <>
     
     <div className="fun-with-text">
         <div className="container">
-            <div className="content-div">
+            <div className="content-div ">
                 <h2>Let's Fun With Text Buddy🤭🤭</h2>
                   
-                  <input type="text" placeholder='First Name' />
-                  <input type="text"placeholder='Last Name' />
-                  <button>Send</button>
+                  <input type="text" placeholder='First Name'  value={firstName} onChange={handlerFirstName}/>
+                  <input  type="text"placeholder='Last Name'  value={lastName} onChange={handlerLastName}/>
+
+                  <button onClick={handleClick}>Send</button>
+                 <div className="output animated-box">
+                     <h2>{fullName}</h2>
+                 </div>
+
 
 
             </div>
