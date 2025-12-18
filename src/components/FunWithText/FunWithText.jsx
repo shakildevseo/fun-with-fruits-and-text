8067cef8df6bc7505ca84158602060645964e089
shakildevseoo <shakildevseoo@gmail.com>
2025-12-18 16:52:12 +0600

    design text-with-fun component

--- src/components/FunWithText/FunWithText.jsx
+++ src/components/FunWithText/FunWithText.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import "../../App.css"
 const FunWithText = () => {
+
+
+
+
+    
   return (
-    <div>FunWithText</div>
+    <>
+    
+    <div className="fun-with-text">
+        <div className="container">
+            <div className="content-div">
+                <h2>Let's Fun With Text Buddy🤭🤭</h2>
+                  
+                  <input type="text" placeholder='First Name' />
+                  <input type="text"placeholder='Last Name' />
+                  <button>Send</button>
+
+
+            </div>
+        </div>
+    </div>
+    
+    
+    </>
   )
 }
 
